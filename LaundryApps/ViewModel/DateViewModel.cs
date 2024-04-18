fbc5dc3dfc432833234ca319dd70bdfbaff4510f
nazirxz <nzryanzr@gmail.com>
2024-04-18 09:26:35 +0700

    change view for order

---
+++ LaundryApps/ViewModel/DateViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LaundryApps.ViewModel
+{
+
+    namespace LaundryApps.ViewModel
+    {
+        public class DateViewModel : INotifyPropertyChanged
+        {
+            private Color _borderColor;
+            public string Day { get; set; }
+            public string Month { get; set; }
+            public string Date { get; set; }
+            public Color BorderColor
+            {
+                get { return _borderColor; }
+                set
+                {
+                    _borderColor = value;
+                    OnPropertyChanged();
+                }
+            }
+            public string NextDay { get; set; }
+            public string NextDate { get; set; }
+            public string NextDay2 { get; set; }
+            public string NextDate2 { get; set; }
+            public string NextDay3 { get; set; }
+            public string NextDate3 { get; set; }
+
+            public DateViewModel()
+            {
+                // Mendapatkan tanggal saat ini
+                var currentDate = DateTime.Now;
+
+                // Mengatur nama hari dalam bahasa Indonesia
+                Day = currentDate.ToString("dddd", new System.Globalization.CultureInfo("id-ID"));
+
+                // Mengatur nama bulan dalam bahasa Indonesia
+                Month = currentDate.ToString("MMMM", new System.Globalization.CultureInfo("id-ID"));
+
+                // Mengatur tanggal
+                Date = currentDate.Day.ToString();
+                NextDay = currentDate.AddDays(1).ToString("dddd", new System.Globalization.CultureInfo("id-ID"));
+                NextDate = currentDate.AddDays(1).Day.ToString();
+                NextDay2 = currentDate.AddDays(2).ToString("dddd", new System.Globalization.CultureInfo("id-ID"));
+                NextDate2 = currentDate.AddDays(2).Day.ToString();
+                NextDay3 = currentDate.AddDays(3).ToString("dddd", new System.Globalization.CultureInfo("id-ID"));
+                NextDate3 = currentDate.AddDays(3).Day.ToString();
+
+                // Set the initial border color to white
+                BorderColor = Color.FromHex("#FFFFFF");
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- LaundryApps/Views/Order.xaml.cs
+++ LaundryApps/Views/Order.xaml.cs
@@ -1,11 +1,53 @@
+using LaundryApps.ViewModel.LaundryApps.ViewModel;
+
 namespace LaundryApps.Views;
 
 public partial class Order : ContentPage
 {
-	public Order()
-	{
-		InitializeComponent();
-	}
+
+    private bool _isSelected = false;
+    public Color SelectedBorderColor { get; set; } = Color.FromHex("#FFFFFF");
+    public Order()
+    {
+
+        InitializeComponent();
+        BindingContext = new DateViewModel();
+
+    }
+    private void BorderTapped(object sender, EventArgs e)
+    {
+        var tappedFrame = (Frame)sender;
+        _isSelected = !_isSelected;
+
+        // Deselect all frames
+        foreach (var frame in new[] { frame1, frame2, frame3, frame4 })
+        {
+            frame.BackgroundColor = Color.FromHex("#FFFFFF");
+        }
+
+        // Select the tapped frame
+        tappedFrame.BackgroundColor = _isSelected ? Color.FromHex("#F691CD") : Color.FromHex("#FFFFFF");
+
+        // If DateViewModel implements INotifyPropertyChanged, trigger the PropertyChanged event
+        // for the BorderColor property of the selected frame
+    }
+    private void TimeTapped(object sender, EventArgs e)
+    {
+        var tappedBorder = (Border)sender;
+        _isSelected = !_isSelected;
+
+        // Deselect all time borders
+        foreach (var border in new[] { jam10, jam12, jam13, jam15 })
+        {
+            border.BackgroundColor = Color.FromHex("#FFFFFF");
+        }
+
+        // Select the tapped border
+        tappedBorder.BackgroundColor = _isSelected ? Color.FromHex("#F691CD") : Color.FromHex("#FFFFFF");
+
+        // If DateViewModel implements INotifyPropertyChanged, trigger the PropertyChanged event
+        // for the SelectedTimeBorderColor property
+    }
     private void btnBack(object sender, EventArgs e)
     {
         // Lakukan navigasi kembali ke halaman sebelumnya di sini
